a85cac77bee6d606548eb4089c426b9076c08cef
Arif Shakil <arif35922@outlook.com>
2023-08-12 08:01:33 +0600

    ng zooro module add

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
-
+import { NgZorroAntdModule } from 'src/app/ng-zorro-antd.module';
 
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [WelcomeRoutingModule,NgZorroAntdModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
